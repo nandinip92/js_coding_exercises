b99643c240c7918f248c234276f563109b3294ec
nandinip92 <nandini.p8892@gmail.com>
2023-07-05 18:43:37 +0100

    fix: Indentation format

--- challenges/exercise001.js
+++ challenges/exercise001.js
@@ -5,94 +5,81 @@
 // Note: Be sure to read the corresponding .md file for each exercise, in the docs folder. 📘 👍
 
 export function capitalize(word) {
-	if (word === undefined) throw new Error('word is required');
-	// Add your code here!
-	return word.charAt(0).toUpperCase()+ word.substr(1,word.length);
+  if (word === undefined) throw new Error("word is required");
+  // Add your code here!
+  return word.charAt(0).toUpperCase() + word.substr(1, word.length);
 }
 
 export function generateInitials(firstName, lastName) {
-	if (firstName === undefined) throw new Error('firstName is required');
-	if (lastName === undefined) throw new Error('lastName is required');
-	// Add your code here!
-	return firstName.charAt(0) +'.' +lastName.charAt(0);
+  if (firstName === undefined) throw new Error("firstName is required");
+  if (lastName === undefined) throw new Error("lastName is required");
+  // Add your code here!
+  return firstName.charAt(0) + "." + lastName.charAt(0);
 }
 
 export function addVAT(originalPrice, vatRate) {
-	if (originalPrice === undefined)
-		throw new Error('originalPrice is requied');
-	if (vatRate === undefined) throw new Error('vatRate is required');
-	// Add your code here!
-	const vatValue = Math.round(originalPrice * vatRate)/100;
-	return originalPrice+vatValue;
+  if (originalPrice === undefined) throw new Error("originalPrice is requied");
+  if (vatRate === undefined) throw new Error("vatRate is required");
+  // Add your code here!
+  const vatValue = Math.round(originalPrice * vatRate) / 100;
+  return originalPrice + vatValue;
 }
 
 export function getSalePrice(originalPrice, reduction) {
-	if (originalPrice === undefined)
-		throw new Error('originalPrice is required');
-	if (reduction === undefined) throw new Error('reduction is required');
-	// Add your code here!
-	const reductionValue = Math.round(originalPrice*reduction)/100;
-	return originalPrice-reductionValue;
+  if (originalPrice === undefined) throw new Error("originalPrice is required");
+  if (reduction === undefined) throw new Error("reduction is required");
+  // Add your code here!
+  const reductionValue = Math.round(originalPrice * reduction) / 100;
+  return originalPrice - reductionValue;
 }
 
 export function getMiddleCharacter(str) {
-	if (str === undefined) throw new Error('str is required');
-	// Add your code here!
-	if (str.length%2 == 0)
-	{
-		return str.substring(str.length/2-1,str.length/2+1);
-	}
-	else
-	{
-		return str.charAt(str.length/2);
-	}
-	
+  if (str === undefined) throw new Error("str is required");
+  // Add your code here!
+  if (str.length % 2 == 0) {
+    return str.substring(str.length / 2 - 1, str.length / 2 + 1);
+  } else {
+    return str.charAt(str.length / 2);
+  }
 }
 
 export function reverseWord(word) {
-	if (word === undefined) throw new Error('word is required');
-	// Add your code here!
-	return word.split("").reverse().join("");
+  if (word === undefined) throw new Error("word is required");
+  // Add your code here!
+  return word.split("").reverse().join("");
 }
 
 export function reverseAllWords(words) {
-	if (words === undefined) throw new Error('words is required');
-	// Add your code here!
-	return words.map(w=> w.split("").reverse().join(""));
+  if (words === undefined) throw new Error("words is required");
+  // Add your code here!
+  return words.map((w) => w.split("").reverse().join(""));
 }
 
 export function countLinuxUsers(users) {
-	if (users === undefined) throw new Error('users is required');
-	// Add your code here!
-	const onlyLinuxUsers = users.filter( u => u["type"] == "Linux");
-	return onlyLinuxUsers.length;
+  if (users === undefined) throw new Error("users is required");
+  // Add your code here!
+  const onlyLinuxUsers = users.filter((u) => u["type"] == "Linux");
+  return onlyLinuxUsers.length;
 }
 
 export function getMeanScore(scores) {
-	if (scores === undefined) throw new Error('scores is required');
-	// Add your code here!
-	const total= scores.reduce((acc,currVal)=> acc + currVal);
-	const result = total/scores.length;
-	return Number(Math.round(result+'e2')+'e-2');
+  if (scores === undefined) throw new Error("scores is required");
+  // Add your code here!
+  const total = scores.reduce((acc, currVal) => acc + currVal);
+  const result = total / scores.length;
+  return Number(Math.round(result + "e2") + "e-2");
 }
 
 export function simpleFizzBuzz(n) {
-	if (n === undefined) throw new Error('n is required');
-	// Add your code here!
-	if ( n%15 == 0)
-	{
-		return "fizzbuzz"
-	}
-	else if (n%3 == 0)
-	{
-		return "fizz";
-	}
-	else if( n%5 == 0)
-	{
-		return "buzz";
-	}
-	else
-	{
-		return n;
-	}
+  if (n === undefined) throw new Error("n is required");
+  // Add your code here!
+  if (n % 15 == 0) {
+    return "fizzbuzz";
+  } else if (n % 3 == 0) {
+    return "fizz";
+  } else if (n % 5 == 0) {
+    return "buzz";
+  } else {
+    return n;
+  }
 }
--- challenges/exercise002.js
+++ challenges/exercise002.js
@@ -1,51 +1,51 @@
 export function getFillings(sandwich) {
-	if (sandwich === undefined) throw new Error('ingredients is required');
-	// Your code here!
-	return sandwich["fillings"];
+  if (sandwich === undefined) throw new Error("ingredients is required");
+  // Your code here!
+  return sandwich["fillings"];
 }
 
 export function isFromManchester(person) {
-	if (person === undefined) throw new Error('person is required');
-	// Your code here!
-	const result = (person['city'] == "Manchester") ? true : false;
-	return result;
+  if (person === undefined) throw new Error("person is required");
+  // Your code here!
+  const result = person["city"] == "Manchester" ? true : false;
+  return result;
 }
 
 export function getBusNumbers(people) {
-	if (people === undefined) throw new Error('people is required');
-	// Your code here!
-	return people<=40 ? 1 : Math.ceil(people/40);
+  if (people === undefined) throw new Error("people is required");
+  // Your code here!
+  return people <= 40 ? 1 : Math.ceil(people / 40);
 }
 
 export function countSheep(arr) {
-	if (arr === undefined) throw new Error('arr is required');
-	// Your code here!
-	//Creating a list of unique values using Set
-	const uniqueArr = new Set(arr);
-	//if (!("sheep" in uniqueArr))
-	//{
-	//	return 0;
-	//}
-	
-	// Creating an empty dictionary to store frequency count
-	let freq={}
-	for ( let value of uniqueArr)
-	{
-		freq[value] = arr.filter(ele => ele == value).length
-	}
-	if('sheep' in freq)
-	{
-		return freq['sheep'];
-	}
-	return 0;
+  if (arr === undefined) throw new Error("arr is required");
+  // Your code here!
+  //Creating a list of unique values using Set
+  const uniqueArr = new Set(arr);
+  //if (!("sheep" in uniqueArr))
+  //{
+  //	return 0;
+  //}
+
+  // Creating an empty dictionary to store frequency count
+  let freq = {};
+  for (let value of uniqueArr) {
+    freq[value] = arr.filter((ele) => ele == value).length;
+  }
+  if ("sheep" in freq) {
+    return freq["sheep"];
+  }
+  return 0;
 }
 
 export function hasMPostCode(person) {
-	if (person === undefined) throw new Error('person is required');
-	// Your code here!
-	if(person.address.postCode.charAt(0)=='M' && person.address.city == 'Manchester')
-	{
-		return true;
-	}
-	return false;
+  if (person === undefined) throw new Error("person is required");
+  // Your code here!
+  if (
+    person.address.postCode.charAt(0) == "M" &&
+    person.address.city == "Manchester"
+  ) {
+    return true;
+  }
+  return false;
 }
