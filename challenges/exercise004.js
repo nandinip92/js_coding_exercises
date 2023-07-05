5105a140378c4667f60b9f45777505697e62c9af
nandinip92 <nandini.p8892@gmail.com>
2023-07-05 22:03:10 +0100

    feat: Added my code using '.forEach' for the given functions (findSmallNums,   findNamesBeginningWith,   findVerbs,   getIntegers,   getCities,   getSquareRoots,   findSentencesContaining,   getLongestSides)

--- challenges/exercise004.js
+++ challenges/exercise004.js
@@ -2,11 +2,11 @@ export function findSmallNums(nums) {
   if (!nums) throw new Error("nums is required");
   // Your code here
   let result = [];
-  for (let i = 0; i < nums.length; i++) {
-    if (nums[i] < 1) {
-      result.push(nums[i]);
+  nums.forEach( function(ele){
+    if (ele < 1) {
+      result.push(ele);
     }
-  }
+  });
   return result;
 }
 
@@ -15,12 +15,12 @@ export function findNamesBeginningWith(names, char) {
   if (!char) throw new Error("char is required");
   // Your code here
   let result = [];
-  for (let i = 0; i < names.length; i++) {
+  names.forEach(item => {
     //comparing the first character of the string with the given charecter
-    if (names[i].charAt(0) == char) {
-      result.push(names[i]);
+    if (item.charAt(0) == char) {
+      result.push(item);
     }
-  }
+  });
   return result;
 }
 
@@ -29,16 +29,16 @@ export function findVerbs(words) {
   // Your code here
 
   let result = [];
-  for (let i = 0; i < words.length; i++) {
+  words.forEach( item =>{
     //splitting the given words by space
-    const wordSplit = words[i].split(" ");
+    const wordSplit = item.split(" ");
 
     // checking if there are more than one elements in the list
     // and the first element is "to"
     if (wordSplit.length > 1 && wordSplit[0] == "to") {
-      result.push(words[i]);
+      result.push(item);
     }
-  }
+  });
   return result;
 }
 
@@ -46,12 +46,12 @@ export function getIntegers(nums) {
   if (!nums) throw new Error("nums is required");
   // Your code here
   let result = [];
-  for (let i = 0; i < nums.length; i++) {
+  nums.forEach( n=> {
     // checking if each element is an integer
-    if (Number.isInteger(nums[i])) {
-      result.push(nums[i]);
+    if (Number.isInteger(n)) {
+      result.push(n);
     }
-  }
+  });
   return result;
 }
 
@@ -60,9 +60,9 @@ export function getCities(users) {
   // Your code here
   let cities = [];
 
-  for (let i = 0; i < users.length; i++) {
-    cities.push(users[i].data.city.displayName);
-  }
+  users.forEach( user => {
+    cities.push(user.data.city.displayName);
+  });
   return cities;
 }
 
@@ -71,12 +71,12 @@ export function getSquareRoots(nums) {
   // Your code here
 
   let squareRoots = [];
-  for (let i = 0; i < nums.length; i++) {
+  nums.forEach( n => {
     //using Math.sqrt function for square roots
-    const sq = Math.sqrt(nums[i]);
+    const sq = Math.sqrt(n);
     const rounded = Number(Math.round(sq + "e2") + "e-2");
     squareRoots.push(rounded);
-  }
+  });
   return squareRoots;
 }
 
@@ -85,16 +85,16 @@ export function findSentencesContaining(sentences, str) {
   if (!str) throw new Error("str is required");
   // Your code here
   let result = [];
-  for (let s = 0; s < sentences.length; s++) {
+  sentences.forEach( s => {
     //converting the given sentence and the string into lowercase since this is not case sensitive
-    let inputSentence = sentences[s].toLowerCase();
+    let inputSentence = s.toLowerCase();
     let givenSubstring = str.toLowerCase();
 
     //Checking if the given string "str" is present in the sentences
     if (inputSentence.includes(givenSubstring)) {
-      result.push(sentences[s]);
+      result.push(s);
     }
-  }
+  });
   return result;
 }
 
@@ -103,18 +103,17 @@ export function getLongestSides(triangles) {
   // Your code here
   let longestSides = [];
 
-  for (let i = 0; i < triangles.length; i++) {
+  triangles.forEach( i => {
     //initialising the max to 0
     let max = 0;
-    for (let j = 0; j < triangles[i].length; j++) {
+    i.forEach( j =>{
       //reassigning the longest side to max
-      if (max < triangles[i][j]) {
-        max = triangles[i][j];
+      if (max < j) {
+        max = j;
       }
-
-    }
+    });
 	//pushing the longest side of this particular Array
 	longestSides.push(max);
-  }
+  });
   return longestSides;
 }
