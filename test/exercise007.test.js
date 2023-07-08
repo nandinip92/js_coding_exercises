83a06d9db7a198a6ffd7df9f853591393588783a
nandinip92 <nandini.p8892@gmail.com>
2023-07-08 22:45:55 +0100

    feat: Written test cases for thr functions sumDigits,   createRange,   getScreentimeAlertList,

---
+++ test/exercise007.test.js
@@ -0,0 +1,198 @@
+import { get } from "request";
+import {
+  sumDigits,
+  createRange,
+  getScreentimeAlertList,
+} from "../challenges/exercise007-optional";
+
+describe("sumDigits", () => {
+  test("Throws error is a string is passed as an input", () => {
+    expect(() => {
+      sumDigits("Booo");
+    }).toThrow("Input must be a number");
+  });
+  test("Returns 0 if the given number is 0", () => {
+    expect(sumDigits(0)).toBe(0);
+  });
+
+  test("Returns sum of the digits in the given number 'n'", () => {
+    expect(sumDigits(123)).toBe(6);
+    expect(sumDigits(8)).toBe(8);
+    expect(sumDigits(395)).toBe(17);
+  });
+  test("Ignores the decimal point if a Float is given as input", () => {
+    expect(sumDigits(56.8)).toBe(19);
+  });
+
+  test("Returns Sum if the given number is less than 0/ negative number", () => {
+    expect(sumDigits(-3)).toBe(3);
+    expect(sumDigits(-57)).toBe(12);
+  });
+});
+
+describe("createRange", () => {
+  test("Throws exceptions when input cpnstraints are not met", () => {
+    expect(() => createRange(-1, 2)).toThrow(
+      "start and end should be greater than 0"
+    );
+    expect(() => createRange(4, -7)).toThrow(
+      "start and end should be greater than 0"
+    );
+    expect(() => createRange(-3, -8)).toThrow(
+      "start and end should be greater than 0"
+    );
+    expect(() => createRange(12, 5)).toThrow(
+      "end should be greater than start"
+    );
+    expect(() => createRange(9, 6)).toThrow("end should be greater than start");
+  });
+
+  test("Return a range of numbers as an array", () => {
+    expect(createRange(3, 11, 2)).toEqual([3, 5, 7, 9, 11]);
+    expect(createRange(5, 15, 3)).toEqual([5, 8, 11, 14]);
+  });
+
+  test(`Check if the optional parameter 'step' is taking the default value 1,
+when it is not provided with input`, () => {
+    expect(createRange(0, 5)).toEqual([0, 1, 2, 3, 4, 5]);
+    expect(createRange(4, 10)).toEqual([4, 5, 6, 7, 8, 9, 10]);
+  });
+
+  test("Returns [start/end] when start===end", () => {
+    expect(createRange(8, 8, 2)).toEqual([8]);
+    expect(createRange(10, 10, 2)).toEqual([10]);
+  });
+});
+
+describe("getScreentimeAlertList", () => {
+  test("return an array of usernames of users who have used more than 100 minutes of screentime for a given date", () => {
+    const users = [
+      {
+        username: "beth_1234",
+        name: "Beth Smith",
+        screenTime: [
+          {
+            date: "2019-05-01",
+            usage: { twitter: 34, instagram: 22, facebook: 40 },
+          },
+          {
+            date: "2019-05-02",
+            usage: { twitter: 56, instagram: 40, facebook: 31 },
+          },
+          {
+            date: "2019-05-03",
+            usage: { twitter: 12, instagram: 15, facebook: 19 },
+          },
+          {
+            date: "2019-05-04",
+            usage: { twitter: 10, instagram: 56, facebook: 61 },
+          },
+        ],
+      },
+      {
+        username: "sam_j_1989",
+        name: "Sam Jones",
+        screenTime: [
+          {
+            date: "2019-06-11",
+            usage: { mapMyRun: 0, whatsApp: 0, facebook: 0, safari: 10 },
+          },
+          {
+            date: "2019-06-13",
+            usage: { mapMyRun: 0, whatsApp: 0, facebook: 0, safari: 16 },
+          },
+          {
+            date: "2019-06-14",
+            usage: { mapMyRun: 0, whatsApp: 0, facebook: 0, safari: 31 },
+          },
+        ],
+      },
+    ];
+
+    const users1 = [
+      {
+        username: "beth_1234",
+        name: "Beth Smith",
+        screenTime: [
+          {
+            date: "2019-05-01",
+            usage: { twitter: 34, instagram: 22, facebook: 40 },
+          },
+          {
+            date: "2019-05-02",
+            usage: { twitter: 56, instagram: 40, facebook: 31 },
+          },
+          {
+            date: "2019-05-03",
+            usage: { twitter: 12, instagram: 15, facebook: 19 },
+          },
+          {
+            date: "2019-05-04",
+            usage: { twitter: 10, instagram: 56, facebook: 61 },
+          },
+          {
+            date: "2019-05-04",
+            usage: { twitter: 10, instagram: 50, facebook: 51 },
+          },
+          {
+            date: "2019-05-04",
+            usage: { twitter: 12, instagram: 15, facebook: 19 },
+          },
+        ],
+      },
+      {
+        username: "sam_j_1989",
+        name: "Sam Jones",
+        screenTime: [
+          {
+            date: "2019-06-11",
+            usage: { mapMyRun: 0, whatsApp: 0, facebook: 0, safari: 10 },
+          },
+          {
+            date: "2019-06-13",
+            usage: { mapMyRun: 0, whatsApp: 0, facebook: 0, safari: 16 },
+          },
+          {
+            date: "2019-06-14",
+            usage: { mapMyRun: 0, whatsApp: 0, facebook: 0, safari: 31 },
+          },
+          {
+            date: "2019-05-04",
+            usage: { twitter: 30, instagram: 50, facebook: 25 },
+          },
+        ],
+      },
+    ];
+
+    expect(getScreentimeAlertList(users, "2019-05-04")).toEqual(["beth_1234"]);
+    expect(getScreentimeAlertList(users1, "2019-05-04")).toEqual([
+      "beth_1234",
+      "sam_j_1989",
+    ]);
+  });
+
+  test("Returns empty array if there are no users who have used more than 100 minutes", () => {
+    const users = [
+      {
+        username: "sam_j_1989",
+        name: "Sam Jones",
+        screenTime: [
+          {
+            date: "2019-06-11",
+            usage: { mapMyRun: 0, whatsApp: 0, facebook: 0, safari: 10 },
+          },
+          {
+            date: "2019-06-13",
+            usage: { mapMyRun: 0, whatsApp: 0, facebook: 0, safari: 16 },
+          },
+          {
+            date: "2019-06-14",
+            usage: { mapMyRun: 0, whatsApp: 0, facebook: 0, safari: 31 },
+          },
+        ],
+      },
+    ];
+
+    expect(getScreentimeAlertList(users, "2019-06-11")).toEqual([]);
+  });
+});
