8f453d3e0f4020c542fd22ca399060e304de1ebe
nandinip92 <nandini.p8892@gmail.com>
2023-07-13 08:08:30 +0100

    Refactor: This commit will refactor of 'hasMPostCode' and 'countSheep' functions with ternary operator. 'isFromManchester' with direct return instead of using result variable.

--- challenges/exercise002.js
+++ challenges/exercise002.js
@@ -7,8 +7,7 @@ export function getFillings(sandwich) {
 export function isFromManchester(person) {
   if (person === undefined) throw new Error("person is required");
   // Your code here!
-  const result = person["city"] === "Manchester" ? true : false;
-  return result;
+  return person["city"] === "Manchester" ? true : false;
 }
 
 export function getBusNumbers(people) {
@@ -32,20 +31,15 @@ export function countSheep(arr) {
   for (let value of uniqueArr) {
     freq[value] = arr.filter((ele) => ele === value).length;
   }
-  if ("sheep" in freq) {
-    return freq["sheep"];
-  }
-  return 0;
+
+  return "sheep" in freq ? freq["sheep"] : 0;
 }
 
 export function hasMPostCode(person) {
   if (person === undefined) throw new Error("person is required");
   // Your code here!
-  if (
-    person.address.postCode.charAt(0) === "M" &&
+  return person.address.postCode.charAt(0) === "M" &&
     person.address.city === "Manchester"
-  ) {
-    return true;
-  }
-  return false;
+    ? true
+    : false;
 }
